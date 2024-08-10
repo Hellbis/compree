40bcb643a2ac1da68caf6783019c9b27f8a06285
hellbis <welder.marques10@gmail.com>
2024-08-10 11:48:00 -0300

    Reorganizando pastas

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
-import { OrdersModule } from './orders/orders.module';
+import { OrdersModule } from './modules/orders/orders.module';
 import { ProductModule } from './products/products.module';
 import { PostgresConfigService } from './config/postgres.config.service';
 import { APP_FILTER } from '@nestjs/core';
-import { GlobalExceptionFilter } from './filters/global-exception-filter';
+import { GlobalExceptionFilter } from './resources/filters/global-exception-filter';
 
 @Module({
   imports: [
